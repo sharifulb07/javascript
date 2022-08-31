b0f4f78ec5576e3fea4e3e995849627616ac67ef
shariful islam <sharifulb07@gmail.com>
2022-08-31 09:08:13 +0600

    dom feature is added here

---
+++ index.js
@@ -0,0 +1,15 @@
+
+function count (){
+    return 5+6;
+}
+
+function test(){
+    
+   
+    document.querySelector("p").innerHTML="no more for today";
+    
+    
+    document.querySelector("h1").innerHTML=count();
+
+}
+
