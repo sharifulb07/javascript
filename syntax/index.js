bc7afde37b66ac08534e74fe6049ae0a7b67a43b
shariful <sharifulb07@gmail.com>
2022-09-04 12:58:25 +0600

    basic mouse event is shown like mousedown, mouseup mouseenter etc.

--- syntax/index.js
+++ syntax/index.js
@@ -1,25 +1,33 @@
-// load, unload
-// scroll
-// resize
-// toggle-details
+// MouseEvent -<html,<head><meta>,<title>,<br>,style,iframe,para, base bd,</title></head>
+// onClick
+// ondblclick
+// onmousedown
+// onmouseup
+// onmouseenter
+// onmouseleave
+// onmousemove
+// onmouseover
 
 
-window.addEventListener("load", function(){
-    console.log("load");
-})
-window.addEventListener("unload", function(){
-    console.log("unload");
-})
-window.addEventListener("scroll", function(){
-    console.log("This is scroll here");
-})
-
-window.addEventListener("resize",function(){
-    var height=this.window.outerHeight;
-    var weight=this.window.outerWidth;
-    console.log(`height: ${height}, width: ${weight}`);
+const div=document.querySelector("div");
+div.addEventListener("click", function(){
+    console.log("click is clicked");
 });
-var details=document.querySelector("details");
-details.addEventListener("toggle",function(e){
-console.log(e.target.open);
-})
+div.addEventListener("dblclick",function(){
+    console.log("double click is occured");
+});
+div.addEventListener("mouseover",function(){
+    console.log("mouse over is occured");
+});
+div.addEventListener("mousedown",function(){
+    console.log("mousedown is occured");
+});
+div.addEventListener("mouseenter",function(){
+    console.log("mouseenter is occured");
+});
+div.addEventListener("mouseleave",function(){
+    console.log("mouseleave is occured");
+});
+div.addEventListener("mousemove",function(){
+    console.log("mousemove is occured");
+});
