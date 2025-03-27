2489ec5c0ef018e46edd1826c9c2054477e35adf
sharifulb07 <sharifulb07@gmail.com>
2025-03-27 11:42:00 +0600

    String in javascript with various tutorial practice

---
+++ welcome.js
@@ -0,0 +1,54 @@
+let str='As sly as a fox , as strong as a ox'
+let target='s'
+
+let pos=0;
+
+
+
+
+
+
+
+// while(true){
+//   let foundAs=str.indexOf(target,pos)
+//   if(foundAs==-1) break;
+//   console.log(`Found at: ${foundAs}`)
+
+//   pos=foundAs+1
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// let name='This is shariful islam who is a man'
+
+// console.log(name.indexOf('is',1))
+// console.log(name.indexOf('is',2))
+// console.log(name.indexOf('is',3))
+// console.log(name.indexOf('is',4))
+
+
+
+
+
+// let str='H'+name[2]
+// console.log(str)
+
+// console.log("INTERFACE".toLocaleLowerCase())
+// console.log('this is lowercase'.toUpperCase());
+
+
+
