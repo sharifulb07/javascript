4cea489309d050d922784246b857bfe953cbd118
sharifulb07 <sharifulb07@gmail.com>
2025-04-02 17:51:00 +0600

    Iterator and Map method like creation, has, get set, clear, size delete etc

--- iterator.js
+++ iterator.js
@@ -1,15 +1,183 @@
 
-let arrlike={
-    0:'Hello',
-    1:'World',
-    length:2
+// let prices = Object.fromEntries([
+//     ['banana', 1],
+//     ['orange', 2],
+//     ['meat', 4]
+//   ]);
+//   console.log(typeof prices)
+
+
+
+
+let obj={
+    name:"Shariful",
+    age:121
 }
 
 
-let result=Array.from(arrlike)
-console.log(Array.isArray(result))
+let newMap=new Map(Object.entries(obj))
 
-console.log(result)
+console.log(Map.toString())
+
+// console.log(newMap.get('name'))
+// console.log(newMap.get('age'))
+
+
+
+
+
+// Iteration over Map
+
+// let receipeMap=new Map([
+//     ['cucumber', 50],
+//     ['tomatoes',300],
+//     ['onion',205]
+// ])
+
+
+// map entries
+// for(let key of receipeMap.entries()){
+//     console.log(key)
+// }
+
+// map values
+
+// for(let key of receipeMap.values()){
+//     console.log(key)
+// }
+
+// map keys
+// for(let key of receipeMap.keys()){
+//     console.log(key)
+// }
+
+
+
+
+// let sharif={name:"Sharif"}
+// let map=new Map();
+// map.set(sharif,123455)
+// map.set(sharif,1255)
+// console.log(map)
+
+
+
+
+
+// we can set any type of data in datastore now 
+// Map data structure here user creating Map, size delete, get, set, has, delete clear
+// let map=new Map();
+
+// map.set(1,'Number');
+// map.set('1',"String");
+// map.set(true,Boolean)
+
+// console.log(map.get(true))
+// console.log(map.size)
+// console.log(map.has(5))
+// console.log(map.has(1))
+// console.log(map.delete(1))
+// console.log(map.has(1))
+
+// map.clear()
+
+// console.log(map.size)
+
+
+
+
+
+
+// let values={
+//     1:"number",
+//     '1':'string'
+// }
+
+// console.log(values[1])
+
+
+
+
+
+
+
+
+
+
+
+// function slice(str, start, end){
+//     return Array.from(str).slice(start,end).join(' ');
+// }
+
+
+// let str='𝒳😂𩷶';
+// let arr=str.split('')
+
+// console.log(arr)
+
+// console.log(slice(str,0,1))
+// console.log(str.slice(1,3))
+
+
+
+
+
+
+// let str='𝒳😂';
+// let char=[];
+
+// for(let c of str){
+//     char.push(c)
+// }
+
+// console.log(char)
+
+
+
+
+
+
+
+
+
+// let range={
+//     from:1,
+//     to:15,
+
+//     [Symbol.iterator](){
+//         this.current=this.from;
+//         return this;
+//     },
+//     next(){
+//         if(this.current<=this.to){
+//             return{done:false, value:this.current++}
+//         }else{
+//             return {done:true}
+//         }
+//     }
+
+
+// }
+
+// let arr=Array.from(range, num=>num+1)
+
+// console.log(arr)
+
+
+
+
+
+// let arrlike={
+//     0:'Hello',
+//     1:'World',
+//     length:2
+// }
+
+
+// let result=Array.from(arrlike)
+// console.log(Array.isArray(result))
+
+// console.log(result)
 
 
 
---
+++ tempCodeRunnerFile.js
@@ -0,0 +1,7 @@
+let prices = Object.fromEntries([
+//     ['banana', 1],
+//     ['orange', 2],
+//     ['meat', 4]
+//   ]);
+//   console.log(typeof prices)
+
